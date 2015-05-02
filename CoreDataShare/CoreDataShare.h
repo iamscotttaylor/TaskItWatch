3a465e60bd226960bbabc465f3b4e81f242cbe51
Scott Taylor <scott@staylor.st>
2015-05-02 17:47:38 +0100

    Adding a CoreDataShare Framework

---
+++ CoreDataShare/CoreDataShare.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataShare.h
+//  CoreDataShare
+//
+//  Created by Scott Taylor on 02/05/2015.
+//  Copyright (c) 2015 Carnaby Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataShare.
+FOUNDATION_EXPORT double CoreDataShareVersionNumber;
+
+//! Project version string for CoreDataShare.
+FOUNDATION_EXPORT const unsigned char CoreDataShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataShare/PublicHeader.h>
+
+
--- TaskItWatch WatchKit Extension/InterfaceController.swift
+++ TaskItWatch WatchKit Extension/InterfaceController.swift
@@ -8,7 +8,7 @@
 
 import WatchKit
 import Foundation
-
+import CoreDataShare
 
 class InterfaceController: WKInterfaceController {
 
